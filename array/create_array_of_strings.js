9d1d8df8a86d31a99652c236561eb1354ca3d8b2
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-03-29 22:19:46 +0530

    Added Creating Arrays from Strings

---
+++ array/create_array_of_strings.js
@@ -0,0 +1,9 @@
+/**
+ *  Creating Arrays from Strings
+ */
+
+ var sentence = "Arrays can be created as the result of calling the split() function on a string";
+ const words = sentence.split(" ");
+ words.forEach(element => {
+     console.log(words);
+ });
